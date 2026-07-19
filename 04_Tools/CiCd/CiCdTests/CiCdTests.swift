e392b26e6e156fdf2b1ace081423aad791f8263f
somsak <somsak.kaeworasan@outlook.com>
2026-07-19 09:13:34 +0700

    Add file.

---
+++ 04_Tools/CiCd/CiCdTests/CiCdTests.swift
@@ -0,0 +1,19 @@
+//
+//  CiCdTests.swift
+//  CiCdTests
+//
+//  Created by tiscomacnb2486 on 18/7/2569 BE.
+//
+
+import Testing
+@testable import CiCd
+
+struct CiCdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
